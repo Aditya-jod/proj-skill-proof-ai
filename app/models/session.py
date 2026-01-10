94f5ee7885961d94ed8ddfa9ab8518b5bd98c7a3
Aditya-jod <aditya1710j@gmail.com>
2026-01-10 22:27:18 +0530

    feat: SQLite login logic input data

--- app/models/session.py
+++ app/models/session.py
@@ -8,7 +8,7 @@ class Session(Base):
     __tablename__ = "sessions"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, unique=True, index=True)
+    user_id = Column(String, index=True)
     mode = Column(String, default="learning") # learning or test
     start_time = Column(DateTime, default=datetime.datetime.utcnow)
     end_time = Column(DateTime)
